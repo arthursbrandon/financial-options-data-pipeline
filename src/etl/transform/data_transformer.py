8688a916edacf8fe8f71b834008ae9e9b61a1ac3
Brandon <brandonarthurs19@gmail.com>
2026-01-28 02:34:16 -0500

    Update

--- src/etl/transform/data_transformer.py
+++ src/etl/transform/data_transformer.py
@@ -20,3 +20,21 @@ class Data_Transformer:
             return df
         except Exception as e:
             print(f'Error: {e}')
+
+    def clean_Options(self, optionData):
+        data = []
+
+        df = pd.DataFrame.from_dict(optionData, orient='columns')
+        exp_dates = df.loc[:,['callExpDateMap','putExpDateMap']].index
+        types = ['callExpDateMap','putExpDateMap']
+
+        for dates in exp_dates:
+            x = df.loc[dates][['callExpDateMap','putExpDateMap']]
+            for type in types:
+                strikes = x[type].keys()
+                for strike in strikes:
+                    optionData.append(x[type][strike][0])
+
+        df = pd.DataFrame(data).drop(columns='optionDeliverablesList')
+        df['expirationDate'] = pd.to_datetime(df['expirationDate']).dt.date
+        return df
--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 from etl.extract import schwab_api
 from etl.transform import data_transformer
 from dotenv import load_dotenv
-import pandas as pd
 import os
 
-import json
-import datetime
-from datetime import datetime,date,time,timedelta
-
+#API Details
 load_dotenv(dotenv_path='../Algorithmic_trading_system/util/.env')
 api_key = os.getenv("APP_KEY")
 api_secret = os.getenv("APP_SECRET")
@@ -16,53 +12,21 @@ api_secret = os.getenv("APP_SECRET")
 api_extract = schwab_api.schwab(api_key,api_secret)
 transformer = data_transformer.Data_Transformer()
 
-ticker_list = ['SPY','QQQ','IWM','GLD','SLV','TQQQ']
+#List of tickers
+ticker_list = ['SPY','QQQ','IWM','GLD','SLV']
 
-try:
-
-    #a = api_extract.getPriceHist('SPY')
-    #df = pd.DataFrame.from_dict(a, orient='columns')
-    #df = df['candles'].apply(pd.Series)
-    
-    
-    a = api_extract.getOptionschain('SPY')
-    df = pd.DataFrame.from_dict(a, orient='columns')
-    exp_dates = df.loc[:,['callExpDateMap','putExpDateMap']].index
-    #calls = df.iloc[0][['callExpDateMap','putExpDateMap']]
-    types = ['callExpDateMap','putExpDateMap']
-    
-    for dates in exp_dates:
-        x = df.loc[dates][['callExpDateMap','putExpDateMap']]
-        for type in types:
-            strikes = x[type].keys()
-            for strike in strikes:
-                print(x[type][strike][0])
-        
-        
-    
-
+#quoteData = transformer.clean_quotes(api_extract.getQuotes(ticker_list))
+def main():
     """
-    #print(df.loc[df.index.isin(calls_dates),])
-    calls = df['callExpDateMap']['2026-01-27:0'].keys
-    print(calls.keys)
-    print(calls)
-    
-    Index(['2026-01-27:0', '2026-01-28:1', '2026-01-29:2', '2026-01-30:3',
-       '2026-02-02:6', '2026-02-03:7', '2026-02-04:8', '2026-02-05:9',
-       '2026-02-06:10', '2026-02-09:13', '2026-02-13:17', '2026-02-20:24',
-       '2026-02-27:31', '2026-03-06:38', '2026-03-20:52'],
-      dtype='str')
+    Docstring for main
+
+    1.) At 9:30 get opening volume and price, download historical data for each ticker in list
+    3.) Define the month trend,low_net_avg,high_net_avg
+    4.)
     """
 
-    
-    """
-    Index(['687.0', '688.0', '689.0', '690.0', '691.0', '692.0', '693.0', '694.0',
-       '695.0', '696.0', '697.0', '698.0', '699.0', '665.0', '670.0', '675.0',
-       '680.0', '685.0', '700.0', '705.0', '710.0', '715.0', '720.0', '725.0']
-    """
+if __name__ == "__main__":
 
-except Exception as e:
-    print(f'Error: {e}')
 
 
 
